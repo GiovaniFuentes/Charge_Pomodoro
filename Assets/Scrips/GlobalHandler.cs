0e8c395a1618d0d29e86980e8dcb3cf741c1a4b1
AdamNMSU <awbluefy@nmsu.edu>
2025-11-17 10:48:19 -0700

    Added Main Page
    Added a main page with a dial that fills and drains when you press a button

---
+++ Assets/Scrips/GlobalHandler.cs
@@ -0,0 +1,47 @@
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GlobalHandler : MonoBehaviour
+{
+    [SerializeField] float currentTimeLeft = 0.0f;
+    [SerializeField] float maxTime = 10.0f;
+    [SerializeField] float startingBreakTime = 5.0f;
+    [SerializeField] float fillRatio =0.02f;
+    [SerializeField] Image radialIndicator = null;
+    public bool TimeFill = false;
+    //awake is called when the object is created directly (I think)
+    void Awake()
+    {
+        //this is to make the object persistent across scenes
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentTimeLeft = startingBreakTime; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TimeFill && currentTimeLeft < maxTime)
+        {
+            currentTimeLeft += fillRatio * Time.deltaTime;
+        }
+        else
+        {
+            if(currentTimeLeft > 0)
+            {
+                currentTimeLeft -= fillRatio * Time.deltaTime;
+            }
+        }
+        radialIndicator.fillAmount = currentTimeLeft / maxTime;
+    }
+
+    public void ToggleTimeFill()
+    {
+        TimeFill = !TimeFill;
+    }
+}
